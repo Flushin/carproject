76f2bf316861fe87a7fcf6322d0df2b08f43c218
flushin <lcsbetel@hotmail.com>
2019-08-22 21:09:43 -0300

    Fix - Remove dead code

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <template>
   <v-app id="sandbox">
-    <v-navigation-drawer
-      v-model="primaryDrawer.model"
-      :clipped="primaryDrawer.clipped"
-      :floating="primaryDrawer.floating"
-      :mini-variant="primaryDrawer.mini"
-      :permanent="primaryDrawer.type === 'permanent'"
-      :temporary="primaryDrawer.type === 'temporary'"
-      app
-      overflow
-    ></v-navigation-drawer>
+    <v-navigation-drawer mini-variant app overflow v-model="toolbar"></v-navigation-drawer>
 
     <v-app-bar
-      :clipped-left="primaryDrawer.clipped"
       app
     >
       <v-app-bar-nav-icon
-        v-if="primaryDrawer.type !== 'permanent'"
-        @click.stop="primaryDrawer.model = !primaryDrawer.model"
+        v-if="toolbar !== 'permanent'"
+        @click.stop="toolbar = !toolbar"
       ></v-app-bar-nav-icon>
-      <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-toolbar-title>Car Project</v-toolbar-title>
     </v-app-bar>
 
     <!--v-content>
@@ -123,14 +113,7 @@ export default {
     components: {
       HelloWorld,
     },
-    drawers: ['Default (no property)', 'Permanent', 'Temporary'],
-    primaryDrawer: {
-      model: null,
-      type: 'default (no property)',
-      clipped: false,
-      floating: false,
-      mini: false,
-    },
+    toolbar: false,
     footer: {
       inset: false,
     },
